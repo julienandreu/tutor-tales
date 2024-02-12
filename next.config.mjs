d8232f4335571d83d1a85fe8b2784ce08381dd5e
Julien Andreu <julienandreu@me.com>
2024-02-11 20:12:05 -0500

    feat: Initial commit

---
+++ next.config.mjs
@@ -0,0 +1,16 @@
+import million from "million/compiler";
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+};
+
+const millionConfig = {
+  auto: true,
+};
+
+export default nextConfig;
+
+// TODO: Replace default export with the following one when the issue is fixed
+// https://github.com/aidenybai/million/pull/959
+// export default million.next(nextConfig, millionConfig);
---
+++ src/app/page.tsx
@@ -0,0 +1,12 @@
+function Home() {
+  return (
+    <div className="flex min-h-screen flex-col items-center justify-between p-24">
+      <h1 className="text-4xl font-bold">Hello World</h1>
+      <p className="text-lg text-center">
+        Welcome to your Next.js + Tailwind CSS app.
+      </p>
+    </div>
+  );
+}
+
+export default Home;
