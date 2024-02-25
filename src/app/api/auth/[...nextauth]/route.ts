86d01c37f51a3e4e26f32e37615d08f64df35ad2
Julien Andreu <julienandreu@me.com>
2024-02-25 18:46:57 -0500

    fix: Bad typing from NextAuth

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -8,6 +8,8 @@ const handler = NextAuth({
       clientSecret: process.env.GOOGLE_CLIENT_SECRET ?? ''
     })
   ]
-});
+}) as unknown;
 
-export { handler as GET, handler as POST };
+export const GET = handler;
+
+export const POST = handler;
